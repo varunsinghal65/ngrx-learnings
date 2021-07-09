eab3877de00e7b2b4b52c9b881489c640046a00a
vsinghal <varun.singhal@amadeus.com>
2021-07-09 15:16:45 +0530

    Importing the StoreModule + create the SST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { ShoppingReducer } from './store/reducers/shopping-reducer';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    FormsModule,
+    //we import a root version of the store module and pass in all the reducers that we have.
+    StoreModule.forRoot({
+      shopping: ShoppingReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/models/app-state.ts
@@ -0,0 +1,7 @@
+//this is your single state tree.
+
+import { ShoppingItem } from "./shopping-item.model";
+
+export interface AppState {
+    readonly shopping: Array<ShoppingItem>
+};
