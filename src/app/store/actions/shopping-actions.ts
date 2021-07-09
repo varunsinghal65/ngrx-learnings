c40bae76b5f02644ec17210aa1e0fa5312e2599d
vsinghal <varun.singhal@amadeus.com>
2021-07-09 18:42:18 +0530

    Added async-friendly actions

--- src/app/store/actions/shopping-actions.ts
+++ src/app/store/actions/shopping-actions.ts
@@ -1,36 +1,77 @@
-
-
 import {Action} from '@ngrx/store'
 import { ShoppingItem } from '../models/shopping-item.model'
 
-//Actions, used whenever we want to update the SST (single state tree) or communicate with our store.
-//While creating action following has to be done:
-/*
-1. create action types enum
-2. implement action interface by creating a class
-3. export the class created
-*/
-
-//action types
+//Let's change our action types to introduce more async-friendly action types
+//As an example, this now gives us the potential to call the LOAD_SHOPPING action
+//and based on the server response, it should either dispatch a LOAD_SHOPPING_SUCCESS 
+//or LOAD_SHOPPING_FAILURE action with a supporting payload.
 export enum ShoppingActionTypes {
-    // string rep of actions, should be unique  
+    LOAD_SHOPPING = '[SHOPPING] Load Shopping',
+    LOAD_SHOPPING_SUCCESS = '[SHOPPING] Load Shopping Success',
+    LOAD_SHOPPING_FAILURE = '[SHOPPING] Load Shopping Failure',
     ADD_ITEM = '[SHOPPING] Add Item',
-    REMOVE_ITEM = '[SHOPPING] Remove Item'
+    ADD_ITEM_SUCCESS = '[SHOPPING] Add Item Success',
+    ADD_ITEM_FAILURE = '[SHOPPING] Add Item Failure',
+    DELETE_ITEM = '[SHOPPING] Delete Item',
+    DELETE_ITEM_SUCCESS = '[SHOPPING] Delete Item Success',
+    DELETE_ITEM_FAILURE = '[SHOPPING] Delete Item Failure'
 }
 
-//creating action
-//Each action has 2 properties - 1. type 2. payload
-export class AddItemAction implements Action {
-    //type
-    readonly type:ShoppingActionTypes = ShoppingActionTypes.ADD_ITEM;
-    //optional - payload
-    constructor(public payload: ShoppingItem) {}
-}
-
-export class RemoveItemAction implements Action {
-    readonly type:ShoppingActionTypes = ShoppingActionTypes.REMOVE_ITEM;
-    constructor(public payload:String){}
-}
-
-//export the actions
-export type ShoppingAction = AddItemAction | RemoveItemAction;
+export class LoadShoppingAction implements Action {
+    readonly type = ShoppingActionTypes.LOAD_SHOPPING
+  }
+  export class LoadShoppingSuccessAction implements Action {
+    readonly type = ShoppingActionTypes.LOAD_SHOPPING_SUCCESS
+  
+    constructor(public payload: Array<ShoppingItem>) {}
+  
+  }
+  export class LoadShoppingFailureAction implements Action {
+    readonly type = ShoppingActionTypes.LOAD_SHOPPING_FAILURE
+    
+    constructor(public payload: Error) {}
+  }
+  
+  export class AddItemAction implements Action {
+    readonly type = ShoppingActionTypes.ADD_ITEM
+  
+    constructor(public payload: ShoppingItem) { }
+  }
+  export class AddItemSuccessAction implements Action {
+    readonly type = ShoppingActionTypes.ADD_ITEM_SUCCESS
+  
+    constructor(public payload: ShoppingItem) { }
+  }
+  export class AddItemFailureAction implements Action {
+    readonly type = ShoppingActionTypes.ADD_ITEM_FAILURE
+  
+    constructor(public payload: Error) { }
+  }
+  
+  export class DeleteItemAction implements Action {
+    readonly type = ShoppingActionTypes.DELETE_ITEM
+  
+    constructor(public payload: string) { }
+  }
+  
+  export class DeleteItemSuccessAction implements Action {
+    readonly type = ShoppingActionTypes.DELETE_ITEM_SUCCESS
+  
+    constructor(public payload: string) { }
+  }
+  export class DeleteItemFailureAction implements Action {
+    readonly type = ShoppingActionTypes.DELETE_ITEM_FAILURE
+  
+    constructor(public payload: string) { }
+  }
+  
+  //export the actions as a ShoppingAction, so that they all can be mapped to the shopping reducer
+  export type ShoppingAction = AddItemAction |
+    AddItemSuccessAction |
+    AddItemFailureAction |
+    DeleteItemAction |
+    DeleteItemSuccessAction |
+    DeleteItemFailureAction |
+    LoadShoppingAction |
+    LoadShoppingFailureAction |
+    LoadShoppingSuccessAction
